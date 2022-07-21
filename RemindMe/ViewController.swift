5e024a2aceaaaf1a48dc140922b6aeddc74cebc4
AhiyaBinal <107073852+AhiyaBinal@users.noreply.github.com>
2022-07-21 15:23:30 -0400

    Initial Commit

---
+++ RemindMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RemindMe
+//
+//  Created by Binal Manek on 2022-07-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
