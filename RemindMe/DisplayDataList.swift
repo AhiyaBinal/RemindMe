20a8e23a7b4148089b0d50a23108533f9cbfebf8
AhiyaBinal <107073852+AhiyaBinal@users.noreply.github.com>
2022-07-21 17:47:42 -0400

    ViewControllers Added

--- RemindMe/DisplayDataList.swift
+++ RemindMe/DisplayDataList.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DisplayDataList.swift
 //  RemindMe
 //
 //  Created by Binal Manek on 2022-07-21.
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DisplayDataList: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnAddPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: "pushToFormViewController", sender: self)
+    }
 }
 
---
+++ RemindMe/InputDataForm.swift
@@ -0,0 +1,29 @@
+//
+//  InputDataForm.swift
+//  RemindMe
+//
+//  Created by Binal Manek on 2022-07-21.
+//
+
+import UIKit
+
+class InputDataForm: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
